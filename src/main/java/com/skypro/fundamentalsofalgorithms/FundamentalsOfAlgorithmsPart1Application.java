99095bf1204bcb4964198758cd0da49996b6c40f
arste <a1ekseevartem.ru@gmail.com>
2022-11-23 23:46:59 +0400

    Initial commit.

---
+++ src/main/java/com/skypro/fundamentalsofalgorithms/FundamentalsOfAlgorithmsPart1Application.java
@@ -0,0 +1,13 @@
+package com.skypro.fundamentalsofalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsOfAlgorithmsPart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalsOfAlgorithmsPart1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/fundamentalsofalgorithms/FundamentalsOfAlgorithmsPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.fundamentalsofalgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsOfAlgorithmsPart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
